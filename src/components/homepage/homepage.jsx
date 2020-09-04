9e4ccdb1294425f2eae6952fc0b9939f4d6be412
Tristan Langford <tristanlangford@hotmail.com>
2020-09-04 20:22:46 +0100

    added description

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -22,10 +22,20 @@ export default class Homepage extends React.Component {
         ]
     }
 
+    randomFontSize() {
+        var num = Math.random()
+        if (num < .1) {
+            num = .15
+        } else if (num > .3 ) {
+            num = .2
+        }
+        return num * 100
+    }
+
     formatTerms() {
         var terms = []
         for(let i = 0; i < this.terms.length; i++) {
-        terms.push(<div className='term'>{this.terms[i]}</div>)
+        terms.push(<div className='term' style={{fontSize:this.randomFontSize()}}>{this.terms[i]}</div>)
         }
         return terms
     }
@@ -34,6 +44,10 @@ export default class Homepage extends React.Component {
         return <div className='homepage-container'>
             <div className='glossary-board'>
                 {this.formatTerms()}
+            </div>
+            <div className='description'>
+                Welcome to Coding Glossary, your online classroom for learning all your techy terminology needed for your Software Engineering interview. While you can code, explaining what HTML is, Composition vs Inheritance or defining SRP can make or break your interview.
+                Use our glossary to learn what all these terms mean, why they exist, and how to explain them to Engineering lead, or non-technical interviewer.
             </div>
         </div>
     }
